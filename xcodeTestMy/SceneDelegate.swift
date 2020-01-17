2bb72f66caf81dfa8658933f93ae7096c00385d7
Sergey <dir@syakovlev.ru>
2020-01-17 17:23:51 +0500

    add main controller in SceneDelegate

--- xcodeTestMy/SceneDelegate.swift
+++ xcodeTestMy/SceneDelegate.swift
@@ -18,6 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = MainViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        window?.windowScene = scene as? UIWindowScene
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
